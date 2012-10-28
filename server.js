e1ba2747cbedf4ba07f9a27d2ec155b60dd57ac0
ewoudj <ewoudj@gmail.com>
2012-10-28 23:07:27 +0100

    First implementation

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-/**
- * Created with JetBrains WebStorm.
- * User: ewoudj
- * Date: 10/28/12
- * Time: 3:49 PM
- * To change this template use File | Settings | File Templates.
- */
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+var options = {
+    router: {
+        'localhost/game': '127.0.0.1:8001',
+        'localhost/': '127.0.0.1:8003'
+    }
+};
+
+var proxyServer = httpProxy.createServer(options);
+
+console.log('Listening on 127.0.0.1:80');
+proxyServer.listen(80);
+
