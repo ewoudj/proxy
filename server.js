3b7f7ebc12e28fe4b3d9d13692435316656069d2
ewoudj <ewoudj@gmail.com>
2012-10-31 00:38:05 +0100

    Undid proxy changes

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var http = require('http'),
 
 var options = {
     router: {
-        '.*?/game': '127.0.0.1:8001',
-        '.*?/': '127.0.0.1:8003'
+        'exarbor.com/game/': '127.0.0.1:8001',
+        'exarbor.com/': '127.0.0.1:8003'
     }
 };
 
