73a1446f16bc16329a2aeb0a9f0ec1ca28c879d9
ewoudj <ewoudj@gmail.com>
2012-10-31 00:16:08 +0100

    More generic routing path (will work in production and debug)

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var http = require('http'),
 
 var options = {
     router: {
-        'exarbor.com/game': '127.0.0.1:8001',
-        'exarbor.com/': '127.0.0.1:8003'
+        '.*?/game': '127.0.0.1:8001',
+        '.*?/': '127.0.0.1:8003'
     }
 };
 
